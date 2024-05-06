1bd71df138cbc4ba42dec7e2b21687f7fbcade2d
Daniel Oh <doh@redhat.com>
2024-05-05 20:08:49 -0600

    RHBQ 3.8, OpenShift 4.15, Dev Spaces 3.13

--- src/test/java/org/acme/people/GreetingResourceTest.java
+++ src/test/java/org/acme/people/GreetingResourceTest.java
@@ -2,11 +2,9 @@ package org.acme.people;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
-import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.startsWith;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -14,10 +12,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
     }
 
     // add more tests
